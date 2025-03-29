f2c196e830c6b165a7b4790883966e119f670214
Mukul data <mukuldata2000@gmail.com>
2025-03-29 17:20:39 +0530

    feat: update add function to handle new line delimiters

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,4 +1,4 @@
 export function add(numbers: string): number {
   if (numbers === "") return 0;
-  return numbers.split(",").map(Number).reduce((a, b) => a + b, 0);
+  return numbers.split(/,|\n/).map(Number).reduce((a, b) => a + b, 0);
 }
