148e3721f4f6f9513598cd5659e32c73d1181d5d
Mukul data <mukuldata2000@gmail.com>
2025-03-29 17:19:58 +0530

    test: add test for handling new line as delimiter

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -13,4 +13,7 @@ describe("String Calculator", () => {
   it("should return the sum of multiple numbers", () => {
     expect(add("1,2,3,4")).toBe(10);
   });
+  it("should handle new lines between numbers", () => {
+    expect(add("1\n2,3")).toBe(6);
+  });
 });
