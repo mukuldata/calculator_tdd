27a255cc9f45bd2a8af933973888305b47c4fee2
Mukul data <mukuldata2000@gmail.com>
2025-03-29 17:33:32 +0530

    test: add test for negative numbers exception

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -19,4 +19,7 @@ describe("String Calculator", () => {
   it("should support different delimiters", () => {
     expect(add("//;\n1;2")).toBe(3);
   });
+  it("should throw an exception for negative numbers", () => {
+    expect(() => add("1,-2,3,-4")).toThrow("negative numbers not allowed -2,-4");
+  });
 });
