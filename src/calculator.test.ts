217a36748c7c3bc8a9d944048c541f2cc9c399cf
Mukul data <mukuldata2000@gmail.com>
2025-03-29 17:45:02 +0530

    test: ensure numbers greater than 1000 are ignored

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -22,4 +22,9 @@ describe("String Calculator", () => {
   it("should throw an exception for negative numbers", () => {
     expect(() => add("1,-2,3,-4")).toThrow("negative numbers not allowed -2,-4");
   });
+  it("ignores numbers greater than 1000", () => {
+    expect(add("2,1001")).toBe(2);
+    expect(add("1000,2")).toBe(1002);
+    expect(add("1001,1002,5")).toBe(5);
+  });
 });
